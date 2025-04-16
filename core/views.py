cec84dcd5a1af3393f9c91f478a9a62b73b659ba
YAOCFOIC <54899583+YAOCFOIC@users.noreply.github.com>
2025-04-15 21:43:59 -0500

    views

--- core/views.py
+++ core/views.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola desde la app core")
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> 3fc74568d910f11ec3eee316e0381bddd6bea6c6
